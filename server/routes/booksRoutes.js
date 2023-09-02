3b8a434816df887699f7c97a7074644af3b6f20d
temtechie <templendukwu144@gmail.com>
2023-09-02 23:36:06 +0100

    implemented update, delete modal and delete

--- server/routes/booksRoutes.js
+++ server/routes/booksRoutes.js
@@ -28,7 +28,7 @@ router.post('/books', async (req, res) => {
 router.get('/books', async (req, res) =>{
     try {
         const books = await Book.find();
-        return res.status(200).json({count: books.length , books});
+        return res.status(200).json(books);
     } catch (error) {
         console.log("Failed to fetch data. Error: ", error.message);
         return res.status(500).json({message: error.message})
